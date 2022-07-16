b17bb60e8f0167d31fc5dd701cc59ffb774e778a
LucasTody3535 <lucas.web3535@gmail.com>
2022-07-16 06:19:25 -0300

    Created basic layout

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,11 +3,33 @@ document.querySelector("#app").dataset.page = "home";
 </script>
 
 <template>
-    <h2>Home works</h2>
+    <nav>
+        Menu
+    </nav>
+    <aside>
+        Kpi
+    </aside>
+    <main>
+        Charts
+    </main>
 </template>
 
 <style>
 [data-page="home"] {
+    background-color: black;
+    display: grid;
+    grid-template-areas:
+    "menu menu menu";
+    grid-template-rows: 10vh 90vh;
+    grid-template-columns: repeat(2, 50%);
+}
+
+[data-page="home"] * {
+    background-color: brown;
+}
+
+[data-page="home"] nav {
     background-color: red;
+    grid-area: menu;
 }
 </style>
